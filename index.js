2395fbba6720e01f7a8a0aead8ca975163e30b2e
micstach <micstach@poczta.onet.pl>
2017-01-14 00:33:22 +0100

    viewport and clipping added'

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var driver = require('node-phantom-simple');
  
 driver.create({ path: require('phantomjs').path }, function (err, browser) {
   return browser.createPage(function (err, page) {
-    page.viewportSize = { width: 1024, height: 768 };
-    page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
-
-    return page.open("http://basketmania.pl/product-pol-9989-Buty-koszykarskie-Galaxy.html", function (err,status) {
-      page.render('output-image.png', function(err){
-          console.log('done');
-      });
+    return page.open("http://finance.yahoo.com", function (err, status) {
+        console.log("Page status: ", status);
+        page.evaluate(function() {
+           document.body.bgColor = 'white';
+        });
+        page.set('clipRect', { top: 0, left: 0, width: 1024, height: 768 }, function () {
+            page.set("viewportSize", {width: 1024, height: 768}, function(){
+                page.render('output-image.png', function(){
+                    console.log("done");
+                    process.exit();
+                });
+            });
+        });
     });
   });
 });
